3895e3c0f7a498dda965979c6ad40f47df6e0d7d
randymfournier <186494476+randymfournier@users.noreply.github.com>
2024-10-31 01:24:44 -0400

    Added a "base" url so that github pages will route the urls properly.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://randymfournier.github.io',
+    base: '/nuclear-neptune'
 });
