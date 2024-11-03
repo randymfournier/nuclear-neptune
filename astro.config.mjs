0c2f35a4b4a9606a9e7aca068ad4c40426122811
randymfournier <186494476+randymfournier@users.noreply.github.com>
2024-11-03 03:12:45 -0500

    Added RSS and Preact Packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://randymfournier.github.io',
-    base: '/nuclear-neptune'
+  site: 'https://randymfournier.github.io',
+  base: '/nuclear-neptune',
+  integrations: [preact()]
 });
